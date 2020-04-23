0858f7aabf8ce28e5cf55aa97a3a15d9b3ad1a6a
Edison <mac@macmatoiMac.local>
2020-04-23 14:45:11 +0800

    edit __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,9 +6,9 @@ from flask import Flask
 def create_app(test_config=None):
     #创建和配置app
     #instance_relative_config=True 告诉应用配置文件是相对于实例文件夹的相对路径。
-    app = Flask(__name__, instance_relative_config=Ture)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev'
+        SECRET_KEY = 'dev',
         #Flask.instance_path 可以找到实例文件夹的绝对路径。
         DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
@@ -27,9 +27,9 @@ def create_app(test_config=None):
         pass
         
     @app.route('/hello')
-        def hello():
-            return 'Hello, World!'
-    
+    def hello():
+        return 'Hello, World!'
+
     return app
         
     
